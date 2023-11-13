d31d983a5a0d6691bddf0ed1ef903a961c91ed6c
I-Frostbyte <irefyre@gmail.com>
2023-11-13 11:03:40 +0100

    Event model done

--- models/Event.ts
+++ models/Event.ts
@@ -1,4 +1,26 @@
 import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator"
 import bcrypt from 'bcrypt';
 
-const 
+const EventSchema = new mongoose.Schema({
+    title: String,
+    Venue: String,
+    Date: Date,
+    Tickets: String,
+}, {
+    timestamps: true, toJSON: { virtuals: true}, toObject: { virtuals: true}
+});
+
+EventSchema.plugin(uniqueValidator, {
+    message: "{PATH} {VALUE} is already in use, please try another",
+});
+
+EventSchema.virtual("artistes", {
+    ref: "Artiste",
+    foreignField: "events",
+    localField: "_id",
+})
+
+const Event = mongoose.model("Event", EventSchema);
+
+export default Event;
