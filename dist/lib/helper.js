c9a2c80d6e65c068a5864029a8acc49da486019a
I-Frostbyte <irefyre@gmail.com>
2023-11-13 22:54:53 +0100

    Final hope

--- dist/lib/helper.js
+++ dist/lib/helper.js
@@ -1,40 +1,30 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAll = exports.getOne = exports.createOne = exports.updateOne = exports.deleteOne = void 0;
 const catchAsync_1 = __importDefault(require("./catchAsync"));
-const deleteOne = (Model) => (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const doc = yield Model.findById(req.params.id);
+const deleteOne = (Model) => (0, catchAsync_1.default)(async (req, res) => {
+    const doc = await Model.findById(req.params.id);
     if (!doc) {
         return res.status(404).json({
             status: "Not found",
             message: `Document with id ${req.params.id} not found`,
         });
     }
-    yield Model.findByIdAndDelete(req.params.id);
+    await Model.findByIdAndDelete(req.params.id);
     return res.status(204).json({
         status: "OK",
         message: "Document deleted successfully",
     });
-}));
+});
 exports.deleteOne = deleteOne;
-const updateOne = (Model, params) => (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const id = (_a = res === null || res === void 0 ? void 0 : res.query) === null || _a === void 0 ? void 0 : _a.id;
+const updateOne = (Model, params) => (0, catchAsync_1.default)(async (req, res) => {
+    const id = res?.query?.id;
     let body = {};
     params.forEach((param) => (body[param] = req.body[param] || null));
-    const updatedDoc = yield Model.findByIdAndUpdate(id, body, {
+    const updatedDoc = await Model.findByIdAndUpdate(id, body, {
         new: true,
         runValidators: true,
     });
@@ -42,19 +32,19 @@ const updateOne = (Model, params) => (0, catchAsync_1.default)((req, res) => __a
         status: "OK",
         data: updatedDoc,
     });
-}));
+});
 exports.updateOne = updateOne;
-const createOne = (Model, params) => (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const createOne = (Model, params) => (0, catchAsync_1.default)(async (req, res) => {
     const body = {};
     params.forEach((param) => (body[param] = req.body[param]));
-    const newDoc = yield Model.create(req.body);
+    const newDoc = await Model.create(req.body);
     return res.status(201).json({
         status: "OK",
         data: newDoc,
     });
-}));
+});
 exports.createOne = createOne;
-const getOne = (Model, populateOptions, selectOptions) => (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const getOne = (Model, populateOptions, selectOptions) => (0, catchAsync_1.default)(async (req, res) => {
     const id = req.params.id;
     let query = Model.findById(id);
     if (populateOptions) {
@@ -62,7 +52,7 @@ const getOne = (Model, populateOptions, selectOptions) => (0, catchAsync_1.defau
     }
     if (selectOptions)
         query = query.select(selectOptions);
-    const doc = yield query;
+    const doc = await query;
     if (!doc) {
         return res.status(404).json({
             status: "Not found",
@@ -73,21 +63,21 @@ const getOne = (Model, populateOptions, selectOptions) => (0, catchAsync_1.defau
         status: "OK",
         data: doc,
     });
-}));
+});
 exports.getOne = getOne;
-const getAll = (Model) => (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const getAll = (Model) => (0, catchAsync_1.default)(async (req, res) => {
     const { page = 1, limit = 10 } = req.query;
     const query = Model.find()
         .limit(Number(limit) * 1)
         .skip((Number(page) - 1) * Number(limit))
         .exec();
-    const count = yield Model.countDocuments();
-    const docs = yield query;
+    const count = await Model.countDocuments();
+    const docs = await query;
     return res.status(200).json({
         status: "OK",
         data: docs,
         totalPages: Math.ceil(count / Number(limit)),
         currentPage: page,
     });
-}));
+});
 exports.getAll = getAll;
--- dist/models/Consumer.js
+++ dist/models/Consumer.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -73,21 +64,19 @@ const ConsumerSchema = new mongoose_1.default.Schema({
 ConsumerSchema.plugin(mongoose_unique_validator_1.default, {
     message: "{PATH} {VALUE} is already in use, please try another",
 });
-ConsumerSchema.pre("save", function (next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            if (!this.isModified("password")) {
-                return next();
-            }
-            const salt = yield bcrypt_1.default.genSalt(10);
-            const hashedPassword = yield bcrypt_1.default.hash(this.password, salt);
-            this.password = hashedPassword;
-            next();
+ConsumerSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
         }
-        catch (error) {
-            return next(error);
-        }
-    });
+        const salt = await bcrypt_1.default.genSalt(10);
+        const hashedPassword = await bcrypt_1.default.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    }
+    catch (error) {
+        return next(error);
+    }
 });
 // ConsumerSchema.pre("save", async function (next) {
 //   const consumer = this;
